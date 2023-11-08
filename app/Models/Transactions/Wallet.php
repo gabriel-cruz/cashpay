b2327d83284ed6c355f12c943047c6cd54fd1c77
Gabriel Cruz <gabrielcruz7400@gmail.com>
2023-11-08 11:37:27 -0300

    feat: creating migrations and models

---
+++ app/Models/Transactions/Wallet.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Transactions;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wallet extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'amount',
+        'user_id',
+    ];
+}
---
+++ database/migrations/2023_11_08_131503_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sender');
+            $table->unsignedBigInteger('receiver');
+            $table->decimal('amount');
+            $table->timestamps();
+
+            $table->foreign('sender')->references('id')->on('users');
+            $table->foreign('receiver')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
